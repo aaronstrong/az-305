5e2aa69284daff96a806790021b1c376944f26e6
Aaron Strong <awstrong@gmail.com>
2025-02-06 08:50:03 -0600

    comment out bucket to destroy it

--- labs/tempdir/main.tf
+++ labs/tempdir/main.tf
@@ -37,10 +37,10 @@ data "azurerm_resource_group" "rg" {
   name = var.existing_rg
 }
 
-resource "azurerm_storage_account" "storageaccount" {
-  name                     = "stg${random_string.uniquestring.result}"
-  resource_group_name      = data.azurerm_resource_group.rg.name
-  location                 = data.azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
+# resource "azurerm_storage_account" "storageaccount" {
+#   name                     = "stg${random_string.uniquestring.result}"
+#   resource_group_name      = data.azurerm_resource_group.rg.name
+#   location                 = data.azurerm_resource_group.rg.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+# }
